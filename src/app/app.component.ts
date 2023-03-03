e9a3995934b5549273a1eb982a71caff5418342f
Mateusz Łędzewicz <mat.ledzewicz@gmail.com>
2023-03-03 11:01:55 +0100

    chore: ignore tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ng-material-starter-kit-acms';
-}
+export class AppComponent {}
